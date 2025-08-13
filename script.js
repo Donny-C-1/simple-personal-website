844837be9e0b98ccbd261ff792f9009ea67762ea
Donny C <chikwemdonald@gmail.com>
2025-08-13 15:13:50 +0100

    Add download link and update tab filtering functionality; include new images and style adjustments

--- script.js
+++ script.js
@@ -19,6 +19,10 @@ function toggleTab() {
         return;
     }
 
+    let prevActiveTabLink = document.querySelector(".tablink.active");
+    prevActiveTabLink.classList.remove("active");
+    this.classList.add("active");
+
     const tabName = this.dataset.target;
     const tabs = document.getElementsByClassName("tab");
 
@@ -32,21 +36,46 @@ function toggleTab() {
 }
 
 function filterGrid() {
-    const imgLoad = imagesLoaded(document.querySelectorAll(".isotope img"));
-    let iso;
-    const options = {
-        itemSelector: ".isotope > *"
-    };
-    imgLoad.on("always", () => (iso = new Isotope(document.querySelector(".isotope"))));
-    const isotopeControlButtons = document.querySelectorAll(".isotope_controls button");
-    let prev = isotopeControlButtons[0];
-    isotopeControlButtons.forEach(
-        el =>
-            (el.onclick = () => {
-                prev.classList.toggle("active");
-                el.classList.toggle("active");
-                prev = el;
-                iso.arrange({ filter: el.dataset.filter === "all" ? "[data-product]" : `[data-product='${el.dataset.filter}']` });
-            })
+    // const imgLoad = imagesLoaded(document.querySelectorAll(".isotope img"));
+    // let iso;
+    // const options = {
+    //     itemSelector: ".isotope > *"
+    // };
+    // imgLoad.on("always", () => (iso = new Isotope(document.querySelector(".isotope"))));
+    // const isotopeControlButtons = document.querySelectorAll(".isotope_controls button");
+    // let prev = isotopeControlButtons[0];
+    // isotopeControlButtons.forEach(
+    //     el =>
+    //         (el.onclick = () => {
+    //             prev.classList.toggle("active");
+    //             el.classList.toggle("active");
+    //             prev = el;
+    //             iso.arrange({ filter: el.dataset.filter === "all" ? "[data-category]" : `[data-category='${el.dataset.filter}']` });
+    //         })
+    // );
+    const controls = document.querySelectorAll(".filter_controls button");
+    let prevActiveControl = document.querySelector(".filter_controls button.active");
+    const blocks = document.querySelectorAll(".filter_grid > *");
+
+    controls.forEach(button =>
+        button.addEventListener("click", () => {
+            prevActiveControl.classList.remove("active");
+
+            prevActiveControl = button;
+            prevActiveControl.classList.add("active");
+
+            let category = button.dataset.filter;
+            blocks.forEach(block => {
+                if (category === "all") {
+                    block.classList.remove("hidden");
+                    return;
+                }
+                if (block.dataset.category === category) {
+                    block.classList.remove("hidden");
+                } else {
+                    block.classList.add("hidden");
+                }
+            });
+        })
     );
 }
